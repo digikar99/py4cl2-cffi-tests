73ea07b9f8ec60e0126afda26ece6fda48668007
digikar99 <shubhamkarayare@gmail.com>
2023-03-07 09:32:19 +0530

    [change] set continue-ignoring-errors to nil in defpymodule

--- package.lisp
+++ package.lisp
@@ -1,16 +1,20 @@
 (in-package :cl-user)
 
-(py4cl2-cffi:defpymodule "math" nil :silent t)
-(py4cl2-cffi:defpymodule "numpy" nil :lisp-package "NP" :silent t)
-(py4cl2-cffi:defpymodule "numpy.random" t :silent t)
+(py4cl2-cffi:defpymodule "math" nil :silent t :continue-ignoring-errors nil)
+(py4cl2-cffi:defpymodule "numpy" nil :lisp-package "NP" :silent t
+                          :continue-ignoring-errors nil)
+(py4cl2-cffi:defpymodule "numpy.random" t :silent t
+                          :continue-ignoring-errors nil)
 
 ;;; Do not test on ECL on CI just to save some travis time
 ;;; Do not test on ABCL because it causes some error even though the command
 ;;; works fine in the REPL
 #-(or ecl abcl)
 (progn
-  (py4cl2-cffi:defpymodule "networkx" nil :lisp-package "NX" :silent t)
-  (py4cl2-cffi:defpymodule "matplotlib.pyplot" nil :lisp-package "PLT" :silent t))
+  (py4cl2-cffi:defpymodule "networkx" nil :lisp-package "NX" :silent t
+                           :continue-ignoring-errors nil)
+  (py4cl2-cffi:defpymodule "matplotlib.pyplot" nil :lisp-package "PLT" :silent t
+                           :continue-ignoring-errors nil))
 
 (defpackage :py4cl2-cffi-tests
   (:use :cl :clunit :py4cl2-cffi :iterate)
